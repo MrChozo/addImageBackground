284b814581bc45705a703f67f6cea200832b5740
Brian <MrChozo@gmail.com>
2018-02-15 12:05:39 -0800

    Add resize function, adjust file verification

--- index.php
+++ index.php
@@ -3,34 +3,34 @@
 
 // Repurposing Imagemagick code I used in another work thing.
 // Basically a Bash script, but eh.
+// Run in parent directory, ~/ in my case.
 
 $dir = './to_convert/';
 $target = './converted/';
+$color = '1c2d3d';
+$height = '600'; // height in px
 
 $filenames = array("Picture29.jpg", "Picture30.jpg", "Picture31.png", "Picture1.jpg", "Picture2.jpg", "Picture3.jpg", "Picture4.jpg", "Picture5.jpg", "Picture6.jpg", "Picture7.jpg", "Picture8.jpg", "Picture9.jpg", "Picture10.jpg", "Picture11.jpg", "Picture12.jpg", "Picture13.jpg", "Picture14.jpg", "Picture15.jpg", "Picture16.jpg", "Picture17.jpg", "Picture18.jpg", "Picture19.jpg", "Picture20.jpg", "Picture21.jpg", "Picture22.jpg", "Picture23.jpg", "Picture24.jpg", "Picture25.jpg", "Picture26.jpg", "Picture27.jpg", "Picture28.jpg"); // Hurk.
 
 
 function verify_file_exists($name) {
-	$test_path = $dir . $name;
-	$target_path = $target . $name;
-	if (!move_uploaded_file($test_path, $target_path)) {
-		echo "Failed to save";
+	$testPath = $dir . $name;
+	if (!file_exists($testPath)) {
+		echo "File doesn't exist: '".$testPath."'.";
 		exit;
 	}
 }
 
-function add_background($name) {
-
-	$path = WWW_ROOT . "/img/employees/" . $name;
+function add_background($name, $color) {
+	$path = $target.$name;
 
 	//Create Thumbnail
 	$thumb_nail = $path . "_thumb.png";
-	$data["filename"] = '/img/employees/' . $name . "_thumb.png";
 
 	$background = $path . '_back.png';
 
 	// Create white background
-	exec("/usr/bin/convert -size 250x250 xc:none $background");
+	exec("/usr/bin/convert -size 250x250 xc:$color $background");
 
 	// Resize image
 	exec("/usr/bin/convert $path -resize '250x250>' -background none -flatten $thumb_nail");
@@ -38,14 +38,23 @@ function add_background($name) {
 	// Combine image onto background
 	exec("/usr/bin/composite -gravity center $thumb_nail $background $thumb_nail");
 
+
+
 	echo "Background added to: '".$name."'.";
+}
+
+// Resize image to appropriate height
+function resize($name, $height) {
+	exec("/usr/bin/convert -geometry x$height $in $out");
 }
 
 // main
 foreach ($filenames as $name) {
 	verify_file_exists($name);
 
-	add_background($name);
+	resize($name, $height);
+
+	add_background($name, $color);
 
 	echo "Completed successfully";
 	exit;
